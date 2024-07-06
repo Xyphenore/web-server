883733d47004061d7a145f1a9572832e9d252f15
Xyphenore <53941443+xyphenore@users.noreply.github.com>
2024-07-06 03:17:28 +0200

    refactor: Reduce the use of memory

--- src/requests/version.rs
+++ src/requests/version.rs
@@ -1,60 +1,91 @@
 use std::fmt::{Display, Formatter};
 use std::ops::Deref;
 
-#[derive(Debug, Clone, PartialEq, PartialOrd, Eq, Ord)]
+#[derive(Debug, Clone, PartialEq, PartialOrd, Eq, Ord, Hash)]
 pub struct Version {
-    code: String,
+    major: char,
+    minor: char,
 }
 
 impl Display for Version {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "HTTP/{}", self.code)
+        if '0' == self.minor {
+            write!(f, "HTTP/{}.{}", self.major, self.minor)
+        } else {
+            write!(f, "HTTP/{}", self.major)
+        }
     }
 }
 
 impl Version {
     pub const HTTP1: Self = Self {
-        code: 1.to_string(),
+        major: '1',
+        minor: '0',
     };
     pub const HTTP1_1: Self = Self {
-        code: 1.1.to_string(),
+        major: '1',
+        minor: '1',
     };
     pub const HTTP2: Self = Self {
-        code: 2.to_string(),
+        major: '2',
+        minor: '0',
     };
     pub const HTTP3: Self = Self {
-        code: 3.to_string(),
+        major: '3',
+        minor: '0',
     };
 
-    const ALLOWED_VERSIONS: Vec<&'_ Self> =
+    const ALLOWED_VERSIONS: Vec<&'static Self> =
         vec![&Self::HTTP1, &Self::HTTP1_1, &Self::HTTP2, &Self::HTTP3];
 
-    pub fn from(value: &str) -> Result<&Self, InvalidHTTPVersionError> {
-        let upper_value = value.to_uppercase();
+    /// Get a constant reference to an HTTP version.
+    ///
+    /// The line must be in the form: HTTP/{version}<br>
+    /// {version} can be 'major' or 'major.minor'
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let version = Version::from("HTTP/1");
+    /// assert!(version.is_ok());
+    /// if let Some(version) = version {
+    ///     assert_eq!(version.to_string(), "HTTP/1");
+    /// }
+    /// ```
+    ///
+    /// ```
+    /// let version = Version::from("1.0");
+    /// assert!(version.is_err());
+    /// if let Err(error) = version {
+    ///     assert_eq!(error, "Invalid HTTP version: '1.0'");
+    /// }
+    /// ```
+    pub fn from(line: impl AsRef<str>) -> Result<&'static Self, InvalidHTTPVersionError> {
+        let upper_line = line.as_ref().to_uppercase();
 
         Self::ALLOWED_VERSIONS
             .iter()
-            .find(|version| version.to_string() == upper_value)
+            .find(|version| version.to_string() == upper_line)
             .and_then(|version| Some(version.deref()))
-            .ok_or(InvalidHTTPVersionError::new(value))
+            .ok_or(InvalidHTTPVersionError::new(line))
     }
 }
 
 #[derive(Debug, Clone)]
-pub struct InvalidHTTPVersionError {
+struct InvalidHTTPVersionError {
     entry: String,
 }
 
 impl Display for InvalidHTTPVersionError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Invalid HTTP version: '{}'", self.entry)
+        write!(f, "Invalid HTTP version: '{}'", &self.entry)
     }
 }
 
 impl InvalidHTTPVersionError {
-    pub fn new(entry: &str) -> Self {
+    fn new(entry: impl AsRef<str>) -> Self {
         Self {
-            entry: entry.to_string(),
+            entry: entry.as_ref().to_string(),
         }
     }
 }
