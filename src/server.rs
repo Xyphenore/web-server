0ac229a2738ab2e2e2ec96ceebacc0c8c60ebca8
Xyphenore <53941443+xyphenore@users.noreply.github.com>
2024-07-08 01:01:47 +0200

    doc: Add documentation for WebServer

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,7 @@
+//! Module providing [`WebServer`], [`enum@Debug`] and [`Method`].
+//!
+//! To see how to create the web server, go to the class [`WebServer`].
+
 use std::collections::HashMap;
 use std::io::ErrorKind::WouldBlock;
 use std::net::{TcpListener, TcpStream};
@@ -8,6 +12,23 @@ pub use crate::requests::Method;
 use crate::requests::{HTTPListener, Request, Response, Status};
 use crate::threads::WorkerPool;
 
+/// The web server.
+///
+/// # How to use it?
+///
+/// ```rust
+/// use crate::server::{WebServer, Debug};
+/// use crate::requests::{Method, Status, Request, Response};
+///
+/// fn process(request: Request) -> Response {
+///     request.make_response_with_status(Status::OK)
+/// }
+///
+/// let server = WebServer::new(5, Debug::False);
+/// server.add_listener(Method::get("/"), process);
+///
+/// server.serve();
+/// ```
 pub struct WebServer {
     #[doc(hidden)]
     cpt: usize,
@@ -20,6 +41,28 @@ pub struct WebServer {
 }
 
 impl WebServer {
+    /// Create the [`WebServer`].
+    ///
+    /// # Parameters
+    ///
+    /// - `amount_workers`: The number must be greater than 0, else panics.
+    /// - `debug`: Activate the debug mode of the server, cf.[`enum@Debug`].
+    ///
+    /// # Returns
+    ///
+    /// Returns a new instance of [`WebServer`].
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use crate::server::{WebServer, Debug};
+    ///
+    /// let server = WebServer::new(5, Debug::False);
+    /// ```
+    ///
+    /// # Panics
+    ///
+    /// - If `amount_workers` is equal to 0.
     pub fn new(amount_workers: usize, debug: Debug) -> WebServer {
         Self {
             cpt: 0,
@@ -29,7 +72,36 @@ impl WebServer {
         }
     }
 
-    pub fn add_listener(&mut self, method: Method, listener: HTTPListener) -> &mut Self {
+    /// Add the route with the [`Method`] and the [`HTTPListener`].
+    ///
+    /// # Parameters
+    ///
+    /// - `method`: The [`Method`] to register.
+    /// The `method` must not be yet registered, else panics.
+    /// - `listener`: The function to process the incoming request for the `method`.
+    ///
+    /// # Returns
+    ///
+    /// Returns the mutable reference to the current instance of [`WebServer`].
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use crate::server::{WebServer, Debug};
+    /// use crate::requests::{Method, Status, Request, Response};
+    ///
+    /// fn process(request: Request) -> Response {
+    ///     request.make_response_with_status(Status::OK)
+    /// }
+    ///
+    /// let server = WebServer::new(5, Debug::False);
+    /// server.add_listener(Method::get("/"), process);
+    /// ```
+    ///
+    /// # Panics
+    ///
+    /// - If the `method` is already registered.
+    pub fn add_listener(&mut self, method: Method, listener: HTTPListener) -> &mut WebServer {
         if self.listeners.contains_key(&method) {
             panic!("A listener is always registered for {}", method)
         }
@@ -39,6 +111,36 @@ impl WebServer {
         self
     }
 
+    /// Execute the server and process incoming requests on `127.0.0.1:8000`.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use crate::server::{WebServer, Debug};
+    /// use crate::requests::{Method, Status, Request, Response};
+    ///
+    /// fn process(request: Request) -> Response {
+    ///     request.make_response_with_status(Status::OK)
+    /// }
+    ///
+    /// let server = WebServer::new(5, Debug::False);
+    /// server.add_listener(Method::get("/"), process);
+    ///
+    /// server.serve();
+    /// ```
+    ///
+    /// # Panics
+    ///
+    /// - If it is not possible to bind the [`TcpListener`] to `127.0.0.1:8000`,
+    /// cf.[`TcpListener::bind()`].
+    /// - If the [`TcpListener`] cannot enter into the non-blocking mode,
+    /// cf.[`TcpListener::set_nonblocking()`].
+    /// - If [`TcpListener::local_addr()`] fails.
+    /// - If [`ctrlc::set_handler()`] fails.
+    /// - If the state `is_running` cannot be locked.
+    /// - If the incoming [`TcpStream`] fails.
+    /// - If the incoming [`TcpStream`] cannot enter into the blocking mode.
+    /// - If the process of the incoming stream, panics.
     pub fn serve(&mut self) {
         let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
         listener
@@ -48,7 +150,7 @@ impl WebServer {
         let is_running = Arc::new(Mutex::new(true));
         println!(
             "Server started and waiting for incoming connections on {}.",
-            listener.local_addr().unwrap()
+            listener.local_addr().unwrap(),
         );
 
         let cloned_is_running = Arc::clone(&is_running);
@@ -73,6 +175,15 @@ impl WebServer {
         }
     }
 
+    /// Process the incoming [`TcpStream`].
+    ///
+    /// # Panics
+    ///
+    /// - If the creation of [`Request`] from the stream, panics.
+    /// cf.[`Request::from_stream()`].
+    /// - If the execution of the request, panics.
+    /// cf.[`WorkerPool::execute()`].
+    #[doc(hidden)]
     fn handle(&mut self, stream: TcpStream) {
         let request = Request::from_stream(stream);
 
@@ -91,6 +202,18 @@ impl WebServer {
             .unwrap();
     }
 
+    /// Process the incoming [`Request`] if any listener is registered for the
+    /// [`Method`] contained in the [`Request`].
+    ///
+    /// # Returns
+    ///
+    /// Returns the response `404 Not Found` with the `templates/not_found.html`
+    /// content.
+    ///
+    /// # Panics
+    ///
+    /// - If [`Response::add_file()`] returns an error.
+    #[doc(hidden)]
     fn not_found_handler(request: Request) -> Response {
         let mut response = request.make_response_with_status(Status::NOT_FOUND);
         response
@@ -101,6 +224,7 @@ impl WebServer {
     }
 }
 
+/// Indicate if the debug mode is activated on the [`WebServer`].
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Debug {
     True,
@@ -108,6 +232,7 @@ pub enum Debug {
 }
 
 impl Debug {
+    /// Get the [`enum@Debug`] enumeration value from the bool value.
     pub fn from(value: bool) -> Debug {
         match value {
             true => Self::True,
