3b20485b39b9425637d12ea13f134c9d871d23dc
Xyphenore <53941443+xyphenore@users.noreply.github.com>
2024-07-07 18:33:35 +0200

    doc: Document get routes

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -2,6 +2,29 @@ use std::path::Path;
 
 use crate::requests::{Request, Response, Status};
 
+/// Process the `GET /`.
+///
+/// # Parameters
+///
+/// - request: [`Request`]
+///
+/// # Returns
+///
+/// [`Response`] - The response to send with [`Response::send()`], with the HTML page
+/// [`templates/index.html`](/templates/index.html).
+///
+/// # Panics
+///
+/// If the method [`Response::add_file()`] returns an error when adding a file.
+///
+/// # Examples
+///
+/// Check examples of [`RequestHandler::add_listener()`][add_listener],
+/// to see how to add the function to process the `GET /`.
+///
+/// <!-- References -->
+///
+/// [add_listener]: crate::requests::RequestHandler::add_listener()
 pub fn get(request: Request) -> Response {
     let mut response = request.make_response_with_status(Status::OK);
     response
--- src/routes/slow_request.rs
+++ src/routes/slow_request.rs
@@ -4,6 +4,32 @@ use std::time::Duration;
 
 use crate::requests::{Request, Response, Status};
 
+/// Process the `GET /slow_request`.
+///
+/// The function sleeps 5 secondes before returning the response, to simulate a slow
+/// request to process.
+///
+/// # Parameters
+///
+/// - request: [`Request`]
+///
+/// # Returns
+///
+/// [`Response`] - The response to send with [`Response::send()`], with the HTML page
+/// [`templates/slow_request.html`](/templates/slow_request.html).
+///
+/// # Panics
+///
+/// If the method [`Response::add_file()`] returns an error when adding a file.
+///
+/// # Examples
+///
+/// Check examples of [`RequestHandler::add_listener()`][add_listener],
+/// to see how to add the function to process the `GET /slow_request`.
+///
+/// <!-- References -->
+///
+/// [add_listener]: crate::requests::RequestHandler::add_listener()
 pub fn get(request: Request) -> Response {
     let mut response = request.make_response_with_status(Status::OK);
     response
