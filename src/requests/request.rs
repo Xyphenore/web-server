2d5c232e5caf74b8c3104cf3d43c2aaf9c1f1bca
Xyphenore <53941443+xyphenore@users.noreply.github.com>
2024-07-08 01:00:10 +0200

    doc: Add documentation for Request

--- src/requests/request.rs
+++ src/requests/request.rs
@@ -3,18 +3,47 @@ use std::net::TcpStream;
 
 use super::{HTTPListener, Job, Method, Response, Status, Version};
 
+/// HTTP request.
+///
+/// # How to create it?
+///
+/// ```rust
+/// use std::net::TcpListener;
+///
+/// use crate::requests::Request;
+///
+/// let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+///
+/// for stream in listener.incoming() {
+///     let request = Request::from_stream(stream.unwrap());
+///
+///     // Process the request after.
+/// }
+/// ```
 #[derive(Debug)]
 pub struct Request {
     pub method: Method,
     #[doc(hidden)]
     version: &'static Version,
+    /// Other lines read from the stream.
     pub _other_lines: Vec<String>,
     #[doc(hidden)]
     stream: TcpStream,
 }
 
 impl Request {
-    pub fn from_stream(stream: TcpStream) -> Self {
+    /// Create a [`Request`] from a [`TcpStream`].
+    ///
+    /// # Returns
+    ///
+    /// Returns a new instance of [`Request`].
+    ///
+    /// # Panics
+    ///
+    /// - If the read of `stream` fails.
+    /// - If the read of the method from the `stream`.
+    /// - If the read of the version from the `stream`.
+    pub fn from_stream(stream: TcpStream) -> Request {
         let buffer_reader = BufReader::new(&stream);
         let mut http_request: Vec<_> = buffer_reader
             .lines()
@@ -39,10 +68,20 @@ impl Request {
         }
     }
 
+    /// Create a [`Response`] with a [`Status`] from the [`Request`] and consume it.
+    ///
+    /// # Returns
+    ///
+    /// Returns a new instance of [`Response`].
     pub fn make_response_with_status(self, status: &'static Status) -> Response {
         Response::new(self.version, status, self.stream)
     }
 
+    /// Create a [`Job`] with a [`HTTPListener`] from the [`Request`] and consume it.
+    ///
+    /// # Returns
+    ///
+    /// Returns a new instance of [`Job`].
     pub fn make_job_with_listener(self, listener: HTTPListener) -> Job {
         Job::new(self, listener)
     }
