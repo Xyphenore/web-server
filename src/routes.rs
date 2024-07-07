c29c62d42b6aef8083fc9c0dff8701ac341e86ea
Xyphenore <53941443+xyphenore@users.noreply.github.com>
2024-07-07 18:48:16 +0200

    doc: Add documentation for route.rs

--- src/routes.rs
+++ src/routes.rs
@@ -1,2 +1,23 @@
+/// The module contains all functions that process requests for the URI `/`.
+///
+/// # Examples
+///
+/// Check examples of [`WebServer::add_listener()`][add_listener],
+/// to see how to add a route to the server.
+///
+/// <!-- References -->
+///
+/// [add_listener]: crate::server::WebServer::add_listener()
 pub mod index;
+
+/// The module contains all functions that process requests for the URI `/slow_request`.
+///
+/// # Examples
+///
+/// Check examples of [`WebServer::add_listener()`][add_listener],
+/// to see how to add a route to the server.
+///
+/// <!-- References -->
+///
+/// [add_listener]: crate::server::WebServer::add_listener()
 pub mod slow_request;
