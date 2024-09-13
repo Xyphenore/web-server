2d82307dcfee541a3dea2663ff9f19816780411c
Xyphenore <53941443+xyphenore@users.noreply.github.com>
2024-09-13 02:17:51 +0200

    feat: Use range-v3 for string operations

--- src/web-server/helpers/string.cpp
+++ src/web-server/helpers/string.cpp
@@ -1,45 +1,72 @@
 #include "string.hpp"
 
+#ifdef USE_RANGES
+    #include <range/v3/algorithm/all_of.hpp>
+    #include <range/v3/range/conversion.hpp>
+    #include <range/v3/view/transform.hpp>
+#endif // USE_RANGES
+
 #include <fmt/base.h>
 
-#include <algorithm>
-#include <execution>
 #include <locale>
 #include <string>
-#include <tuple>
+
+#ifndef USE_RANGES
+    #include <algorithm>
+    #include <execution>
+    #include <tuple>
+#endif // USE_RANGES
 
 namespace web_server::helpers {
+#ifndef USE_RANGES
     std::string to_uppercase(const fmt::string_view value) noexcept {
         std::string upper_value{};
         upper_value.resize(value.size());
 
-        const std::locale locale{};
+        static const std::locale locale{};
+        static const auto to_upper = [](const std::string::value_type c) { return std::toupper(c, locale); };
 
         std::ignore = std::transform(
-#ifdef __cpp_lib_execution
+    #ifdef __cpp_lib_execution
             std::execution::par_unseq,
-#endif // __cpp_lib_execution
+    #endif // __cpp_lib_execution
             value.begin(),
             value.end(),
             upper_value.begin(),
-            [&locale](const std::string::value_type c) { return std::toupper(c, locale); });
+            to_upper);
 
         return upper_value;
     }
 
     bool is_blank(const fmt::string_view value) noexcept {
-        const std::locale locale{};
+        static const std::locale locale{};
+        static const auto is_blank = [](const fmt::string_view::value_type ch) {
+            return std::isblank(ch, locale) or std::isspace(ch, locale);
+        };
 
-        const auto* const last_left_blank_char = std::find_if_not(
-#ifdef __cpp_lib_execution
+        return std::all_of(
+    #ifdef __cpp_lib_execution
             std::execution::par_unseq,
-#endif // __cpp_lib_execution
+    #endif // __cpp_lib_execution
             value.begin(),
             value.end(),
-            [&locale](const fmt::string_view::value_type ch) {
-                return std::isblank(ch, locale) or std::isspace(ch, locale);
-            });
-
-        return value.end() == last_left_blank_char;
+            is_blank);
     }
+#else // USE_RANGES
+    std::string to_uppercase(const fmt::string_view value) noexcept {
+        static const std::locale locale{};
+        static const auto to_upper = [](const fmt::string_view::value_type c) { return std::toupper(c, locale); };
+
+        return value | ranges::views::transform(to_upper) | ranges::to<std::string>;
+    }
+
+    bool is_blank(const fmt::string_view value) noexcept {
+        static const std::locale locale{};
+        static const auto is_blank = [](const fmt::string_view::value_type ch) {
+            return std::isblank(ch, locale) or std::isspace(ch, locale);
+        };
+
+        return ranges::all_of(value, is_blank);
+    }
+#endif // USE_RANGES
 } // namespace web_server::helpers
